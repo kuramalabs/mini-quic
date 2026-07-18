70bfae3804e2de6bbf565855c2068d06e83354fe
Rohit <rohitnarnolia88@gmail.com>
2026-07-18 10:33:00 +0530

    add encode and decode

---
+++ src/helpers/mod.rs
@@ -0,0 +1,21 @@
+use crate::MessageType;
+
+pub fn decode_message(bytes: &[u8]) -> Result<(MessageType, &[u8]), &str> {
+    let msg_type = match bytes.first() {
+        Some(0) => MessageType::Join,
+        Some(1) => MessageType::Regular,
+        Some(_) => return Err("Unknown message type"),
+        None => return Err("Empty packet"),
+    };
+
+    Ok((msg_type, &bytes[1..]))
+}
+
+pub fn encode_message(msg_type: MessageType, payload: &[u8]) -> Vec<u8> {
+    let mut buf = Vec::with_capacity(1 + payload.len());
+
+    buf.extend_from_slice(&u8::from(msg_type).to_be_bytes());
+    buf.extend_from_slice(payload);
+
+    buf
+}
---
+++ src/types/mod.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tokio::time::Instant;
+
+pub type Clients = Arc<Mutex<HashMap<SocketAddr, Instant>>>;
+
+#[derive(Debug, PartialEq)]
+pub enum MessageType {
+    Join,
+    Regular,
+}
+
+impl From<MessageType> for u8 {
+    fn from(value: MessageType) -> Self {
+        match value {
+            MessageType::Join => 0,
+            MessageType::Regular => 1,
+        }
+    }
+}
