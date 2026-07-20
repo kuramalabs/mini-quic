25bfbab3107d4f9cfd8d6fce7b541f3f607d3b06
Rohit <rohitnarnolia88@gmail.com>
2026-07-20 22:19:05 +0530

    feat: handle seq_no ejection on client side

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,11 +1,27 @@
 use mini_quic::{decode_message, encode_message, MessageType};
+use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::net::UdpSocket;
+use tokio::sync::Mutex;
+use tokio::time::Instant;
 
+// need something to track ack based messages
+struct PendingMessage {
+    encoded: Vec<u8>,
+    sent_at: Instant,
+}
+
+type Pending = Arc<Mutex<HashMap<u32, PendingMessage>>>;
+impl PendingMessage {
+    pub fn new(encoded: Vec<u8>, sent_at: Instant) -> Self {
+        Self { encoded, sent_at }
+    }
+}
 struct Sender {
     client: Arc<UdpSocket>,
     message_no: u32,
+    pending: HashMap<u32, PendingMessage>,
 }
 
 impl Sender {
@@ -13,6 +29,7 @@ impl Sender {
         Self {
             client: Arc::new(UdpSocket::bind("0.0.0.0:0").await.unwrap()),
             message_no: 0,
+            pending: HashMap::new(),
         }
     }
 
@@ -21,7 +38,7 @@ impl Sender {
             println!("Failed to connect to the server: {}", e);
         }
     }
-    pub async fn send_message(&mut self, msg: &[u8], is_regular: bool) {
+    pub async fn send_message(&mut self, msg: &[u8], is_regular: bool, pending: Pending) {
         self.message_no += 1;
         let msg_type = match is_regular {
             true => MessageType::Regular,
@@ -29,10 +46,21 @@ impl Sender {
         };
         // encode the sequence number
 
-        let msg = encode_message(msg_type, msg, self.message_no);
+        let msg = encode_message(msg_type.clone(), msg, self.message_no);
+
         if let Err(e) = self.client.send(&msg).await {
             println!("Failed to deliver the message to server : {}", e);
+            return;
         }
+
+        if msg_type != MessageType::Join {
+            pending
+                .lock()
+                .await
+                .insert(self.message_no, PendingMessage::new(msg, Instant::now()));
+        }
+
+        // Now how to copy here
     }
 }
 #[tokio::main]
@@ -40,22 +68,43 @@ async fn main() {
     let mut sender = Sender::new().await;
     sender.form_connection().await;
 
-    let c1 = sender.client.clone();
+    let pending: Pending = Pending::new(Mutex::new(HashMap::new()));
 
+    let c1 = sender.client.clone();
+    let p1 = pending.clone();
     tokio::spawn(async move {
         let mut buf = vec![0; 256];
         loop {
             let r = c1.recv(&mut buf).await.unwrap();
             let msg = decode_message(&buf[..r]).unwrap();
-            println!(
-                "Received: Seq Number: {}, Message: {}",
-                msg.1,
-                String::from_utf8_lossy(msg.2)
-            );
+
+            match msg.0 {
+                MessageType::Regular => {
+                    println!(
+                        "Received: Seq Number: {}, Message: {}",
+                        msg.1,
+                        String::from_utf8_lossy(msg.2)
+                    );
+                }
+                MessageType::Ack => {
+                    // eject the seq number received from the server
+                    match p1.lock().await.remove(&msg.1) {
+                        Some(_) => {
+                            println!("Removed the seq_no {} as ack received.", msg.1)
+                        }
+                        None => {
+                            println!("Seq number not found")
+                        }
+                    }
+                }
+                _ => {
+                    todo!()
+                }
+            }
         }
     });
 
-    sender.send_message(b"", false).await;
+    sender.send_message(b"", false, pending.clone()).await;
     println!("Join message sent");
 
     let stdin = tokio::io::stdin();
@@ -70,6 +119,8 @@ async fn main() {
         if msg.is_empty() {
             continue;
         }
-        sender.send_message(msg.as_bytes(), true).await;
+        sender
+            .send_message(msg.as_bytes(), true, pending.clone())
+            .await;
     }
 }
